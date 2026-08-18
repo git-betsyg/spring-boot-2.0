db5a5e01fa8b62da6b212ec68151b40f68c7fc24
Betsyg Richards <48546154+git-betsyg@users.noreply.github.com>
2026-08-18 15:11:57 +0800

    feat: 添加Spring Security和OAuth2资源服务器支持，配置JWT认证和用户管理

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public String index(Authentication authentication) {
+        return "Hello, " + authentication.getName() + "!";
+    }
 
     @GetMapping("/hello")
     public String hello(@RequestParam(defaultValue = "world") String name) {
